4d8fe57fafb78b529581c2d043737db3475531dc
timka_byzov <byz0vtm@gmail.com>
2022-05-19 10:49:20 +0500

    Merge remote-tracking branch 'origin/AddLocationOfObject' into FireAndObjects

--- try { escape() }/Assets/Scripts/DragAndDrop.cs
+++ try { escape() }/Assets/Scripts/DragAndDrop.cs
@@ -7,12 +7,12 @@ using UnityEngine.EventSystems;
 public class DragAndDrop : MonoBehaviour
 {
     private bool dragging;
-    private bool touchingBound;
     private float EPS = 0.01f;
-    public GameObject bound;
-    private Vector3 lastDir;
     private Rigidbody2D rigidBody;
     private bool onPosition;
+    private Vector2 lastNotNullVelocity;
+    private Vector2 objectPos;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,33 +23,32 @@ public class DragAndDrop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        // var t = Vector2.up;
+        // var u = transform.TransformPoint(new Vector2(0, 0));
+        // print(u.x.ToString() + " " + u.y.ToString() + " " + u.z.ToString());
+        // rigidBody.AddForceAtPosition(t * 3, u);
+        //rigidBody.AddForce(Vector2.up);
         CheckClick();
-
         var mousePos = (Vector2) Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var position = (Vector2) transform.position;
+        var position = (Vector2) transform.TransformPoint(objectPos);
         if (dragging && !onPosition)
         {
-            var dir = (mousePos - position);
-            lastDir = dir;
-
-            var dist = Vector2.Distance(mousePos, position);
-
-            transform.position = position + dir.normalized * Mathf.Min(dist, 0.12f);
-            //rigidBody.AddForce(new Vector2((distX) * Math.Sign(-transform.position.x + mousePos.x), 0));
-
-
-            //  var distX = Math.Abs(position.x - mousePos.x);
-            //  var distY = Math.Abs(position.y - mousePos.y);
-            //  var forceDirX = Math.Sign(-position.x + mousePos.x) * distX * distX;
-            //  var forceDirY = Math.Sign(-position.y + mousePos.y) * distY * distY; 
-            //
-            // rigidBody.AddForceAtPosition(new Vector2(forceDirX, forceDirY), mousePos);
-
-
-            //lastPos = transform.position;
+            var dir = mousePos - position;
+            lastNotNullVelocity = dir.normalized * Vector2.Distance(mousePos, position) * 25;
+            
+            var t = dir.normalized * Mathf.Pow(Vector2.Distance(mousePos, position) * 0.1f, 1);
+            //rigidBody.AddForceAtPosition(t, position, ForceMode2D.Impulse);
+            rigidBody.velocity = lastNotNullVelocity;
+            
+            
         }
-
-        if (Math.Abs(position.x - mousePos.x) < EPS && Math.Abs(position.y - mousePos.y) < EPS) onPosition = true;
+        
+        if (Vector2.Distance(mousePos, position) < EPS)
+        {
+            onPosition = true;
+            rigidBody.velocity = Vector2.zero;
+        }
         else
         {
             onPosition = false;
@@ -57,58 +56,28 @@ public class DragAndDrop : MonoBehaviour
     }
 
 
-    private Vector2 ClipForce()
-    {
-        var lastDirForce = lastDir.magnitude;
-
-        //if (lastDirForce < 0.01f) return Vector2.zero;
-        
-        //if (lastDirForce < 0.05) return 100 * lastDir.normalized;
-        if (lastDirForce > 20) return 750 * lastDir.normalized;
-        return lastDir * 200;
-    }
-
-
     private void CheckClick()
     {
-        if (Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0) && dragging)
         {
-            if (!dragging) return;
-            dragging = false;
             rigidBody.gravityScale = 1;
-
-            rigidBody.AddForce(ClipForce());
-            return;
-        }
-
-        if (!Input.GetMouseButtonDown(0))
-        {
-            return;
-        }
-
-
-        var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        var hit = Physics2D.Raycast(mousePos, Vector2.zero);
-        if (hit.collider == null) return;
-
-        if (hit.collider.gameObject == gameObject)
-        {
-            rigidBody.gravityScale = 0;
-            rigidBody.velocity = Vector2.zero;
-            dragging = true;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject == bound)
-        {
-            // if (dragging)
-            //     transform.position = lastPos;
-            print("oulala");
-            // lastPos = transform.position;
-            // touchingBound = true;
+            rigidBody.velocity = lastNotNullVelocity;
             dragging = false;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var hit = Physics2D.Raycast(mousePos, Vector2.zero);
+            if (hit.collider == null) return;
+
+            if (hit.collider.gameObject == gameObject)
+            {
+                //rigidBody.gravityScale = 0;
+                dragging = true;
+                objectPos = transform.InverseTransformPoint(mousePos);
+                return ;
+            }
         }
     }
 }
